e5bfae24cd64f01355aad59a577ed992c84fd7e0
thinh199536student <n23dcvt082@student.ptithcm.edu.vn>
2025-11-18 23:43:20 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ export default async function handler(req) {
     // --------------------------------
     const apiUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" +
-      process.env.GEMINI_API_KEY;
+      process.env.GOOGLE_API_KEY;
 
     const apiRes = await fetch(apiUrl, {
       method: "POST",
@@ -103,6 +103,7 @@ export default async function handler(req) {
     );
   }
 }
+
 
 
 
