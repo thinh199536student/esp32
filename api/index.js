0271defcb090c3132e05c7b4c7d6adc63f5ea348
thinh199536student <n23dcvt082@student.ptithcm.edu.vn>
2025-11-18 23:33:23 +0700

    Update API endpoint to use new model version

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ export default async function handler(req) {
     // CALL GOOGLE GEN-LANG TTS API
     // --------------------------------
     const apiUrl =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-robotics-er-1.5-preview:generateSpeech?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateSpeech?key=" +
       process.env.GOOGLE_API_KEY;
 
     const apiRes = await fetch(apiUrl, {
@@ -103,5 +103,6 @@ export default async function handler(req) {
     );
   }
 }
+
 
 
